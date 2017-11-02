f5d73968efffd131954815dabad10c4cfadf4187
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-11-02 17:59:04 +0100

    add "CanStayInFrontOfFunction"

--- funcs/Util.cs
+++ funcs/Util.cs
@@ -5,9 +5,16 @@ namespace MTAResourceStats.funcs {
 		private static HashSet<char> canstaybehindset = new HashSet<char> {
 			',', ' ', '\n', ';', ')', '(', '"', '\'', '[', ']', '{', '}', '='
 		};
+		private static HashSet<char> canstayinfrontoffunction = new HashSet<char> {
+			'(', ' ', '\n'
+		};
 
 		public static bool CanStayBehind ( char character ) {
 			return canstaybehindset.Contains ( character );
+		}
+
+		public static bool CanStayInFrontOfFunction ( char character ) {
+			return canstayinfrontoffunction.Contains ( character );
 		}
 	}
 }
