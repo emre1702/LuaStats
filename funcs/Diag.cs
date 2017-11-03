5705a2669142fbb60b2ca9e4318a8854fdcc9274
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-11-03 19:11:55 +0100

    add class for easier diagnostics with Stopwatch

---
+++ funcs/Diag.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace MTAResourceStats.funcs {
+	class Diag {
+		private Stopwatch watch;
+		private string str = "";
+	
+		public Diag ( ) {
+			this.watch = new Stopwatch ();
+			this.watch.Start ();
+		}
+
+		public void SaveTick ( string info ) {
+			watch.Stop ();
+			this.str += ( info + watch.ElapsedTicks ) + "\n";
+			watch.Restart ();
+		}
+
+		public void SaveSeconds ( string info ) {
+			watch.Stop ();
+			this.str += ( info + ( (double) watch.ElapsedMilliseconds / 1000 ) ) + "\n";
+			watch.Restart ();
+		}
+
+		public void End ( bool output = false ) {
+			watch.Stop ();
+			watch = null;
+			if ( output )
+				Message.SendInfoMessage ( this.str );
+		}
+	}
+}
