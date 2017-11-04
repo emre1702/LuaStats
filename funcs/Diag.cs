34bad71fec20551e4446b7b181ccf6c317204ad0
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-11-04 14:34:26 +0100

    add possibility to add values for index (for multiple-files diag)

--- funcs/Diag.cs
+++ funcs/Diag.cs
@@ -1,40 +1,75 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using MTAResourceStats.enums;
 
 namespace MTAResourceStats.funcs {
 	class Diag {
 		private Stopwatch watch;
 		private string str = "";
+		private Dictionary<string, long> timeperindex;
 		private bool output;
+		private DiagOption option;
 	
-		public Diag ( bool output ) {
+		public Diag ( bool output, DiagOption option = DiagOption.startToEnd ) {
 			this.output = output;
 			if ( output ) {
 				this.watch = new Stopwatch ();
-				this.watch.Start ();
+				if ( option == DiagOption.startToEnd ) {
+					this.watch.Start ();
+				} else if ( option == DiagOption.indexValue ) {
+					this.timeperindex = new Dictionary<string, long> ();
+				}
 			}
+			this.option = option;
+		}
+
+		public void Start ( ) {
+			this.watch.Start ();
+		}
+		
+		public void Stop ( ) {
+			this.watch.Reset ();
+			this.watch.Stop ();
 		}
 
 		public void SaveTick ( string info ) {
 			if ( this.output ) {
-				watch.Stop ();
-				this.str += ( info + watch.ElapsedTicks ) + "\n";
-				watch.Restart ();
+				this.watch.Stop ();
+				if ( this.option == DiagOption.startToEnd ) {
+					this.str += ( info + this.watch.ElapsedTicks ) + "\n";
+				} else if ( this.option == DiagOption.indexValue ) {
+					timeperindex[info] = ( timeperindex.ContainsKey ( info ) ? timeperindex[info] + this.watch.ElapsedTicks : this.watch.ElapsedTicks );
+				}
+				this.watch.Restart ();
 			}
 		}
 
 		public void SaveSeconds ( string info ) {
 			if ( this.output ) {
-				watch.Stop ();
-				this.str += ( info + ( (double) watch.ElapsedMilliseconds / 1000 ) ) + "\n";
-				watch.Restart ();
+				this.watch.Stop ();
+				if ( this.option == DiagOption.startToEnd ) {
+					this.str += ( info + ( (double) this.watch.ElapsedMilliseconds / 1000 ) ) + "\n";
+					this.watch.Restart ();
+				} else if ( this.option == DiagOption.indexValue ) {
+					timeperindex[info] = ( timeperindex.ContainsKey ( info ) ? timeperindex[info] + this.watch.ElapsedMilliseconds : this.watch.ElapsedMilliseconds );
+					this.watch.Reset ();
+				}
 			}
 		}
 
 		public void End ( ) {
 			if ( this.output ) {
-				watch.Stop ();
-				watch = null;
-				Message.SendInfoMessage ( this.str );
+				this.watch.Stop ();
+				this.watch = null;
+				if ( this.option == DiagOption.startToEnd ) {
+					Message.SendInfoMessage ( this.str );
+				} else if ( this.option == DiagOption.indexValue ) {
+					string newstr = "";
+					foreach ( KeyValuePair<string, long> entry in this.timeperindex ) {
+						newstr += entry.Key + ": " + entry.Value + "\n";
+					}
+					Message.SendInfoMessage ( newstr );
+				}
 			}
 		}
 	}
